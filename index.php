954c34ccc97572116ec185de403868b9b91788f7
Azhar Uddin <azhar@sender.net>
2024-02-03 13:48:57 +0600

    hide id col but sort desc

--- index.php
+++ index.php
@@ -140,7 +140,14 @@ if (isset($_GET['code'])) {
     <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
     <script>
         new DataTable('#url-table', {
-            order: [[0, 'desc']]
+            order: [[0, 'desc']],
+            columnDefs: [
+                {
+                    target: 0,
+                    visible: false,
+                    searchable: false
+                }
+            ]
         });
     </script>
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
